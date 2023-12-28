157b99fce9facf2900c8f3bd9bf2ef19b059c97f
nsNeruno <infinia249@gmail.com>
2023-12-28 17:54:16 +0700

    Removed ARC check

--- SDWebImage/Core/SDWebImageCompat.m
+++ SDWebImage/Core/SDWebImageCompat.m
@@ -8,10 +8,10 @@
 
 #import "SDWebImageCompat.h"
 
-#if !__has_feature(objc_arc)
-    #error SDWebImage is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
-#endif
+// #if !__has_feature(objc_arc)
+//     #error SDWebImage is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
+// #endif
 
-#if !OS_OBJECT_USE_OBJC
-    #error SDWebImage need ARC for dispatch object
-#endif
+// #if !OS_OBJECT_USE_OBJC
+//     #error SDWebImage need ARC for dispatch object
+// #endif
